c530e264e8da44507cf2300cfded14b662b140d1
lenny_cheng <langrisser1981@gmail.com>
2022-11-28 22:29:22 +0800

    Initial Commit

---
+++ TomatoClock/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TomatoClock
+//
+//  Created by 程信傑 on 2022/11/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TomatoClock/TomatoClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomatoClockApp.swift
+//  TomatoClock
+//
+//  Created by 程信傑 on 2022/11/28.
+//
+
+import SwiftUI
+
+@main
+struct TomatoClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
